4ce6a7629c03da188afdb5d2e3ff72dd75366e97
Martin Skalický <skalicky@modernalt.eu>
2024-02-27 23:08:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # Function to clone the project from GitHub
 clone_project() {
-    echo "Cloning project from GitHub..."
+    echo "Cloning project from GitHub into $SCRIPTS_DIR..."
     git clone https://github.com/maskalix/cmnds/tree/main/scripts%20 "$SCRIPTS_DIR"
     echo "Project cloned successfully."
 }
@@ -16,16 +16,20 @@ initialize_project() {
     echo "Project initialized successfully."
 }
 
+# Function to prompt user for SCRIPTS_DIR
+prompt_scripts_dir() {
+    read -rp "Enter preferred directory for scripts (default: /data/scripts): " SCRIPTS_DIR
+    SCRIPTS_DIR=${SCRIPTS_DIR:-"/data/scripts"}
+}
+
 # Main function to execute installation process
 install_project() {
+    prompt_scripts_dir
     initialize_project
     clone_project
 
     echo "Installation completed successfully."
 }
-
-# Set the default SCRIPTS_DIR
-SCRIPTS_DIR="/data/scripts"
 
 # Run installation process
 install_project
