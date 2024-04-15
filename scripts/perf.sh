1b0c135e0d17f0b9855fb60df0b4444bb94499c5
Martin Skalický <skalicky@modernalt.eu>
2024-04-15 18:35:04 +0200

    Update perf.sh

--- scripts/perf.sh
+++ scripts/perf.sh
@@ -150,7 +150,7 @@ display_speed_test() {
     local result_url=$(echo "$speedtest_result" | grep "Result URL" | awk '{print $3}')
     
     echo "╔═════════════════╦═════════════════╦═════════════════╗"
-    echo "║ Download        ║ Upload          ║ Ping            ║"
+    echo "║  Upload         ║ Download        ║ Ping            ║"
     echo "╠═════════════════╬═════════════════╬═════════════════╣"
     printf "║ \033[1;33m%-17s\033[0m ║ \033[0;32m%-17s\033[0m ║ %-15s ║\n" "↑ $upload_speed Mbps" "↓ $download_speed Mbps" "$ping ms"
     echo "╚═════════════════╩═════════════════╩═════════════════╝"
