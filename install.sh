f7dc6f7c411322e2acfa427a768b9401f6e9822b
Martin Skalický <skalicky@modernalt.eu>
2024-05-13 13:07:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,6 +56,8 @@ clone_project() {
     msg_info "Cloning project from GitHub into $SCRIPTS_DIR..."
     git clone --depth 1 --filter=tree:0 https://github.com/maskalix/cmnds.git "$SCRIPTS_DIR" > /dev/null 2>&1
     cd "$SCRIPTS_DIR" || exit 1
+    git log --format=%cd --date=format-local:"%Y-%m-%d %H:%M:%S" -1 > version
+    cd "$SCRIPTS_DIR" || exit 1
     git sparse-checkout set --no-cone scripts > /dev/null 2>&1
     msg_success "Project cloned successfully."
 }
