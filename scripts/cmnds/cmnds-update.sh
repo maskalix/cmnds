811d694358b22ab3975d020fa5b2067c4c001c58
Martin Skalický <skalicky@modernalt.eu>
2024-10-11 23:34:06 +0200

    Update cmnds-update.sh

--- scripts/cmnds/cmnds-update.sh
+++ scripts/cmnds/cmnds-update.sh
@@ -31,12 +31,9 @@ else
     mkdir "$temp_dir" || { echo "Failed to create $temp_dir directory."; exit 1; }
 fi
 
-# Change to $HOME/cmnds-temp directory
-cd "$temp_dir" || { echo "Failed to change to $temp_dir directory."; exit 1; }
-
 # Download and execute install script
 display_header "${YELLOW}Downloading and executing install script${NC}"
-wget --no-cache -q https://raw.githubusercontent.com/maskalix/cmnds/main/install.sh && chmod +x install.sh && ./install.sh
+wget --no-cache -q https://raw.githubusercontent.com/maskalix/cmnds/main/install.sh -P $temp_dir && chmod +x $temp_dir/install.sh && $temp_dir/install.sh
 
 # Move back to parent directory
 cd - >/dev/null || { echo "Failed to change to parent directory."; exit 1; }
