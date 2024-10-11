e02c7f7be3811895792a0bc29581e367523eb4a5
Martin Skalický <skalicky@modernalt.eu>
2024-10-11 23:58:27 +0200

    Update cmnds-update.sh

--- scripts/cmnds/cmnds-update.sh
+++ scripts/cmnds/cmnds-update.sh
@@ -33,7 +33,13 @@ fi
 
 # Download and execute install script
 display_header "${YELLOW}Downloading and executing install script${NC}"
-wget --no-cache -q https://raw.githubusercontent.com/maskalix/cmnds/main/install.sh -P $temp_dir && chmod +x $temp_dir/install.sh && $temp_dir/install.sh && rm $temp_dir/install.sh
+wget --no-cache -q https://raw.githubusercontent.com/maskalix/cmnds/main/install.sh -P $temp_dir && chmod +x $temp_dir/install.sh 
+if [ -e "$temp_dir/install.sh" ]; then
+    echo "Running script $temp_dir/install.sh"
+    $temp_dir/install.sh && rm $temp_dir/install.sh
+else
+    echo "Error downloading install script"
+fi
 
 # Move back to parent directory
 cd - >/dev/null || { echo "Failed to change to parent directory."; exit 1; }
