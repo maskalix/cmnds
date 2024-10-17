e675ec92ccd769036576d8de0ff42511d2d3681f
Martin Skalický <skalicky@modernalt.eu>
2024-10-18 00:28:12 +0200

    Update cpc.sh

--- scripts/files/cpc.sh
+++ scripts/files/cpc.sh
@@ -24,17 +24,27 @@ transfer_files() {
     local source_dir="$1"
     local target_dir="$2"
     local subfolder_name=$(basename "$source_dir")
-    
+
     # Create target subfolder in transfer directory if it doesn't exist
     mkdir -p "$target_dir/$subfolder_name"
     echo "Transfer directory created: $target_dir/$subfolder_name"
-    
+
+    # Get the total number of files to be copied
+    total_files=$(find "$source_dir" -type f | wc -l)
+    current_file=0
+
     # Copy files from source subfolder to target subfolder
     for file in "$source_dir"/*; do
         if [ -f "$file" ]; then
-            cp "$file" "$target_dir/$subfolder_name/"
+            ((current_file++))
+            rsync --progress "$file" "$target_dir/$subfolder_name/"
+            
+            # Calculate progress percentage
+            percentage=$((100 * current_file / total_files))
+            echo -ne "Progress: $percentage% ($current_file/$total_files)\r"
         fi
     done
+    echo -ne "\n" # Move to a new line after progress is complete
 }
 
 # Ask for main folder path
@@ -54,7 +64,7 @@ create_transfer_directory "$transfer_dir"
 # Loop through subfolders of main folder
 for subfolder in "$main_folder"/*; do
     if [ -d "$subfolder" ]; then
-        # transfer files from each subfolder with progress
+        # Transfer files from each subfolder with progress
         transfer_files "$subfolder" "$transfer_dir"
     fi
 done
