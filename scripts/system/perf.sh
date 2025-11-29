747f1a4db186e921fae112b93baa4f8e3b3b2ecc
Martin Skalický <skalicky@modernalt.eu>
2025-11-29 19:57:53 +0100

    Update perf.sh

--- scripts/system/perf.sh
+++ scripts/system/perf.sh
@@ -57,8 +57,9 @@ if ! command -v speedtest &> /dev/null; then
         sudo apt-get install curl
     fi
     echo -e "${GREEN}Speedtest is not installed. Installing...${NC}"
-    curl -L "https://ip1.info/gist_dl/index.php?gist_id=299338de5b9a8a7d40a0f0c48a9f47a6&filename=speedtest-cli-install.sh" | sudo bash
-    sudo apt-get install -y speedtest
+
+    sudo apt-get install curl
+    curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
     echo -e "Preparing speedtest..."
     speedtest --accept-gdpr &> /dev/null
 else
